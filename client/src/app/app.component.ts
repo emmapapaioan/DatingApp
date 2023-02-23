220d351c12abec7ffb4664523c388f8d66064579
Emma Papaioannou <emmapapaioannou@outlook.com>
2023-02-23 14:31:47 +0200

    Update issue fixed

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -7,12 +7,12 @@ import { AccountService } from './_services/account.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'Dating Application';
 
   // http and accountService properties are injected into the component's constructor 
   // using Angular's dependency injection mechanism
-  constructor(private accountService: AccountService){}
+  constructor(private accountService: AccountService) { }
 
   ngOnInit(): void {
     this.setCurrentUser();
@@ -22,9 +22,10 @@ export class AppComponent implements OnInit{
   // as the current user of the AccountService
   setCurrentUser() {
     const userString = localStorage.getItem('user');
-    if(!userString) return;
-    const user: any = JSON.stringify(userString); // ***any should change to User
-    this.accountService.setCurrentUser(user);
+    if (userString) {
+      const user: User = JSON.parse(userString);
+      this.accountService.setCurrentUser(user);
+    }
   }
 
 }
